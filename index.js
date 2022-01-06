a021ff48141a9ad3d2db53ad256d63b7649048a6
Jared <jbeckersalax9@aol.com>
2022-01-06 14:41:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+
+function createEmployeeRecord(employeeInfo){
+    let employeeObj = {
+        firstName:employeeInfo[0],
+        familyName:employeeInfo[1],
+        title:employeeInfo[2],
+        payPerHour:parseInt(employeeInfo[3]),
+        timeInEvents:[],
+        timeOutEvents:[]
+        }
+    return employeeObj
+}
+createEmployeeRecord(testEmployee)
+
+function createEmployeeRecords(employeeAray){
+    employeeAray.forEach(employee => {
+        createEmployeeRecord(employee)
+    })
+}
+
+createEmployeeRecords(twoRows)
+
